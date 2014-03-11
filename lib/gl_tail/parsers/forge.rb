52df2dddfc79ab170ca86e13ac474f1e2cb04224
Conor Keegan <conor.keegan@bbc.co.uk>
2014-03-11 11:52:54 +0000

    host changes

--- lib/gl_tail/parsers/forge.rb
+++ lib/gl_tail/parsers/forge.rb
@@ -18,10 +18,21 @@ class ForgeParser < Parser
       #add_activity(:block => 'sites', :name => server.name, :size => size.to_i) # Size of activity based on size of request
       add_activity(:block => 'urls', :name => url)
       add_activity(:block => 'users', :name => app, :size => size.to_i + time.to_i)
-      add_activity(:block => 'hosts', :name => machine)
+      if machine.include?("54.246") | machine.include?("176.34")
+	  subnet = "AWS"  
+      elsif machine.include?("195.74")
+          subnet = "BDS"      
+      elsif machine.include?("172.23")      
+          subnet = "Forge"  
+      elsif machine.include?("132.185")
+          subnet = "RBM Zone"
+      else
+          subnet = machine
+      end
+      add_activity(:block => 'hosts', :name => subnet)
 
       type = 'page'
-      add_activity(:block => 'content', :name => method)
+      add_activity(:block => 'methods', :name => method)
       add_activity(:block => 'status', :name => status, :type => 3) # don't show a blob
 
       add_activity(:block => 'warnings', :name => "#{status}: #{url}") if status.to_i > 400
